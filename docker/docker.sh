56d211aaba2e2c90550dd80ea15879f05979e770
madmazoku <madmazoku@gmail.com>
2018-04-05 19:31:03 +0300

    update docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,7 @@
 echo ========= Build docker image
 docker build -t otus.lessons.22.01 .
-echo ========= Execute async
-perl -e 'print "cmd$_\n" foreach 1..10;' | docker run --rm -i otus.lessons.22.01 async_cli 4
+# echo ========= Execute bulk_server
+# docker run --rm -i otus.lessons.22.01 
+# ( ./bulk_server 9999 3 ) & (sleep 1 ; seq 1 10 | nc localhost 9999 ; pkill -INT bulk_server)
 echo ========= Remove docker image
 docker rmi otus.lessons.22.01
